e6641393f8df06425496858d7e790078f642a8b8
Madhuri <madhuribs111@gmail.com>
2024-12-31 22:42:36 +0530

    checked routes

--- backend/routes/index.route.js
+++ backend/routes/index.route.js
@@ -1,7 +1,7 @@
 import express from "express"
 import pinRoute from "../routes/pin.route.js";
 import userRoute from "../routes/user.route.js"
-const indexRouter = express()
+const indexRouter = express.Router()
 
 app.use("/pins", pinRoute);
 app.use("/user", userRoute)
