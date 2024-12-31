8cd81badf867be7b431dbad9960b8e4c3c20ab49
Madhuri <149250958+madhuribs111@users.noreply.github.com>
2024-12-31 22:17:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.options('*', (req, res) => {
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
-  res.sendStatus(204); // No content
+  res.sendStatus(200); 
 });
 
 // Main route
